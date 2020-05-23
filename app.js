2b790f1982f1c3abdb41a383d5b1dd71705cbbbf
OmidMahiar <omidraihany90@gmail.com>
2020-05-23 21:41:12 +0430

    changed the port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = 9000;
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '/public')));
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
