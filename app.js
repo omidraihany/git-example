5b6047302f83cbd2604d52332c2769e57b26d1d2
OmidMahiar <omidraihany90@gmail.com>
2020-05-23 21:13:51 +0430

    first commite

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const app = express();
+const port = 3000;
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, '/public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+hbs.registerHelper('date', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('uppercase', (text) => {
+    return text.toUpperCase();
+})
+app.get('/', (req, res) => {
+    // res.send('Hello to my webSite!');
+    res.render('home', {
+        pageTitle: 'barghkonur.com',
+        title: 'Omid Reihany'
+    });
+})
+app.listen(port, () => {
+    console.log(`'I'm running on servert with port ${port}`);
+})
